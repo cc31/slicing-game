3270385ab4357bb3c52ce4bc54734ad553208bba
Chelsea Chan <cc987@cornell.edu>
2020-09-28 23:33:24 +0800

    implemented tapping and random movement

--- game.js
+++ game.js
@@ -1,58 +1,66 @@
-let score = 0;
-let lives = 10;
+let score;
+let lives;
 let fruits = [];
 let gameState = false;
 
 function setup() {
     createCanvas(400,300);
-    frameRate(15);
-
-    for (let i = 0; i < 3; i++) {
-        // create new fruit
-        let x = random(width);
-        let y = height;
-        let r = random(10,30);
-        let c = color(random(0,255), random(0,255), random(0,255));
-        let f = new Fruit(x, y, r, c);
-        fruits.push(f);
-    }
+    frameRate(30);
+    lives = 10;
+    score = 0;
 }
 
 function draw() {
-
     background(0);
 
-    // Display Score and Lives
-    fill(0, 102, 160, 60);
-    text('Score: ' + score.toString(), 10, 290);
-    text('Lives: ' + lives.toString(), 250, 290);
-
-    for (let i = 0; i < fruits.length; i++) {
-        fruits[i].move();
-        fruits[i].show();
-        // if sliced, score++
-        if (fruits[i].sliced) {
-            score++;
-        } 
-        // .. if escaped, score--
-        if (fruits[i].escaped) {
-            fruits.splice(i);
-            lives--;
-        }
-    }
-    
     drawSlice();
+    drawFruit();
+    drawScore();
     
     // if lives = 0, endgame
     if (lives < 1) {
-        // endGame();
+        endGame();
     }
+}
+
+function drawScore(){
+    // Display Score and Lives
+    noStroke();
+    fill("#ffffff");
+    text('Score: ' + score.toString(), 10, 20);
+    noStroke();
+    fill("#ffffff");
+    text('Lives: ' + lives.toString(), width-60, 20);
 }
 
 function drawSlice(){
     stroke(126);
     line(mouseX, mouseY, pmouseX, pmouseY);
-    print(pmouseX + ' -> ' + mouseX);
+    // print(pmouseX + ' -> ' + mouseX);
+}
+
+function drawFruit(){
+
+    if (frameCount % 30 === 0) {
+        let x = random(width);
+        let y = height;
+        let r = random(10,30);
+        let c = color(random(100,255), random(100,255), random(100,255));
+        let f = new Fruit(x, y, r, c);
+        fruits.push(f);
+    }
+
+    for (let i = 0; i < fruits.length; i++) {
+        fruits[i].move();
+        fruits[i].show();
+        if (fruits[i].clicked()) {
+            score++;
+        }
+        if (fruits[i].escaped) {
+            fruits.splice(i,1);
+            lives--;
+        }
+    }
 }
 
 class Fruit {
@@ -62,13 +70,15 @@ class Fruit {
         this.r = r;
         this.c = c;
         this.veloX = randomVeloX(x);
-        this.veloY = random(-5, -30);
+        this.veloY = random(-10, -15);
+        this.escaped = false;
     }
 
     clicked(px, py){
         let d1 = dist(px, py, this.x, this.y);
         if (d1 < this.r){
             score++;
+            this.r = 0;
         }
     }
 
@@ -78,23 +88,18 @@ class Fruit {
 
     move(){
         this.veloX *= 0.99; // air resistance
-        this.veloY += 2; // gravity
-        this.x = this.x + this.veloX;
-        this.y = this.y + this.veloY;
+        this.veloY += 0.5; // gravity
+        this.x += this.veloX;
+        this.y += this.veloY;
+        if (this.y > height) {
+            this.escaped = true;
+        }
     }
 
     show(){
         noStroke();
         fill(this.c);
         circle(this.x, this.y, this.r*2);
-    }
-
-    escaped(){
-        if (this.y > height){
-            return true;
-        } else {
-            return false;
-        }
     }
 }
 
@@ -106,9 +111,9 @@ function mousePressed() {
 
 function randomVeloX(x) {
     if (x > width / 2) {
-        return random(-10, -1);
+        return random(-4, -0.5);
     } else {
-        return random(1, 10);
+        return random(0.5, 4);
     }
 }
 
@@ -118,9 +123,9 @@ function endGame() {
   
     textAlign(CENTER);
     noStroke();
-    fill("#888888");
+    fill("#999999");
     textSize(30);
     text("game over", width / 2, height / 2);
     textSize(10);
     text("Press f5 to restart!", width / 2, height / 2 + 30);
-  }
+}
